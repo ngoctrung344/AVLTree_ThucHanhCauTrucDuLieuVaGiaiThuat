131409bc87efee7c310092f21e82ccd7cb58eac7
ngoc trung <ngoctrung1a9@gmail.com>
2025-05-13 14:45:32 +0700

    them lai avl

---
+++ AVL/AVL/Header.h
@@ -0,0 +1,14 @@
+﻿#include "iostream"
+using  namespace std;
+const int max = 100;
+
+typedef int typeinfo;
+struct Node {
+	typeinfo info;
+	Node* left, *right;
+	typeinfo height;
+};
+typedef Node* tree;
+
+
+void init(tree& r);
---
+++ File/Source.cpp
@@ -0,0 +1,10 @@
+#include "Header.h"
+
+NODE taoNode(int x) {
+	NODE p = new NODE();
+	p.key
+}
+int nodeHeight(NODE* node);
+int getHeight(NODE* root);
+NODE leftRotate(NODE* root);
+NODE Insert(NODE* root, int x);
